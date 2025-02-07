f5f8416e23f509babfb6ed0a6e68b598a4fe1b42
BinChenLAN <102538198+BinChenLAN@users.noreply.github.com>
2025-02-07 23:14:36 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,7 +8,8 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+ sed -i '$a src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
+ sed -i '$a src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
