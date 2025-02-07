f28e6d83d26d19eef36fc814efa28c058227dd75
BinChenLAN <102538198+BinChenLAN@users.noreply.github.com>
2025-02-07 22:52:22 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -24,7 +24,8 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 #
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
+svn co modem https://github.com/FUjr/modem_feeds.git
+svn co istore https://github.com/linkease/istore
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
